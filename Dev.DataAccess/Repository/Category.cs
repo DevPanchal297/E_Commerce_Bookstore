d9a6b3d2c29399630ea44e6a1b3c4fbf6293282a
Dev Panchal <devpanchal1452@gmail.com>
2025-09-10 00:06:05 +0530

    feat: Generic REPO

---
+++ Dev.DataAccess/Repository/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dev.DataAccess.Data;
+using Dev.DataAccess.Repository.IRepository;
+
+namespace Dev.DataAccess.Repository
+{
+    public class Category : Repository<Category> ,ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public Category(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
